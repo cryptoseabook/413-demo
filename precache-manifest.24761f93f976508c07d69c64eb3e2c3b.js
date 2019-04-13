8db23fffdff05e349bc13028b5dc472ae8b0598f
seabook chen <cryptoseabook@gmail.com>
2019-04-13 17:06:49 +1000

    Updates

---
+++ precache-manifest.24761f93f976508c07d69c64eb3e2c3b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "77f91b5fcad366cb40bb",
+    "url": "/413-demo/static/js/runtime~main.9d1685bd.js"
+  },
+  {
+    "revision": "6e19747959c0ec572ec6",
+    "url": "/413-demo/static/js/main.91750814.chunk.js"
+  },
+  {
+    "revision": "07ea4914f5788873d90a",
+    "url": "/413-demo/static/js/2.695279fd.chunk.js"
+  },
+  {
+    "revision": "6e19747959c0ec572ec6",
+    "url": "/413-demo/static/css/main.0ed536b8.chunk.css"
+  },
+  {
+    "revision": "0797b10f2da298a97ec45afe3acf5ceb",
+    "url": "/413-demo/index.html"
+  }
+];
